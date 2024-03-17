446271849bb33b2e86956f51e71be9554c0cc1f9
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-03-17 17:51:40 +1100

    Remove init.py

--- app/app.py
+++ app/app.py
@@ -1,8 +1,17 @@
-from init import create_app
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask import Flask, jsonify
+from routes import init_routes
 
-app = create_app()
+# creates an application that is named after the name of the file
+app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "some_dev_key"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://mario:mypassword@pgsql:5432/todos"
+
+# initializing routes
+init_routes(app)
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
